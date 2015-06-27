4be146c0365b339fb719993e422ec08175b7152c
Ronen Moldovan <ronen.moldovan@gmail.com>
2015-06-27 22:37:40 +0300

    Added additional keys

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ronenm:offerjar-i18n-official',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'i18n translation to OfferJar negotiation',
   // URL to the Git repository containing the source code for this package.
